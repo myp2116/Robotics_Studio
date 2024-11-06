9756e4192b222a41e2f82bcb741846516aa97c87
myp2116 <152449759+myp2116@users.noreply.github.com>
2024-11-05 20:38:24 -0500

    Add files via upload

--- Punisher_code.py
+++ Punisher_code.py
@@ -19,9 +19,13 @@ try:
     
     #Right Side Servos
     servoR101 = LX16A(101)
+    servoR101.set_angle_limits(129, 161)
     servoR102 = LX16A(102)
+    servoR102.set_angle_limits(120, 137)
     servoR103 = LX16A(103)
+    servoR103.set_angle_limits(129, 161)
     servoR104 = LX16A(104)
+    servoR104.set_angle_limits(120, 137)
 
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
@@ -29,24 +33,25 @@ except ServoTimeoutError as e:
 
 #Homing Code
 #Home ALL servos
-L1_home = 135 #Degrees
-L2_home = 127.5 #Degrees
-L3_home = 135 #Degrees
-L4_home = 127.5 #Degrees
+L1_home = 145 #Degrees
+L2_home = 128.5 #Degrees
+L3_home = 145 #Degrees
+L4_home = 128.5 #Degrees
 
-#R101_home = 
-#R102_home =
-#R103_home =
-#R104_home =
-print(servoR101.get_physical_angle())
-print(servoR102.get_physical_angle())
-print(servoR103.get_physical_angle())
-print(servoR104.get_physical_angle())
+R101_home = 145
+R102_home = 128.5
+R103_home = 145
+R104_home = 128.5
 
 servoL1.move(L1_home)
 servoL2.move(L2_home)
 servoL3.move(L3_home)
 servoL4.move(L4_home)
+
+servoR101.move(R101_home)
+servoR102.move(R102_home)
+servoR103.move(R103_home)
+servoR104.move(R104_home)
 
 t = 0
 while True:
@@ -58,7 +63,11 @@ while True:
     servoL3.move(sin(t) * 15 + 145)
     servoL4.move(sin(t) * 8.5 + 128.5)
 
-
-
+    #Front right Side Leg
+    #servoR101.move(cos(t) * 15 + 145)
+    #servoR102.move(cos(t) * 8.5 + 128.5)
+    #Back Right Side leg
+    #servoR103.move(cos(t) * 15 + 145)
+    #servoR104.move(cos(t) * 8.5 + 128.5)
     time.sleep(0.05)
     t += 0.1
