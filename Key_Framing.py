146853e08cc06351b00db0c0919dffef593bf8a7
myp2116 <152449759+myp2116@users.noreply.github.com>
2024-12-15 16:49:19 -0500

    Add files via upload

--- Key_Framing.py
+++ Key_Framing.py
@@ -20,13 +20,13 @@ try:
     
     #Right Side Servos
     servoR101 = LX16A(101)
-    #servoR101.set_angle_limits(129, 161)
+    servoR101.set_angle_limits(0, 240)
     servoR102 = LX16A(102)
-    #servoR102.set_angle_limits(120, 137)
+    servoR102.set_angle_limits(0, 240)
     servoR103 = LX16A(103)
-    #servoR103.set_angle_limits(129, 161)
+    servoR103.set_angle_limits(0, 240)
     servoR104 = LX16A(104)
-    #servoR104.set_angle_limits(120, 137)
+    servoR104.set_angle_limits(0, 240)
 
 except ServoTimeoutError as e:
     print(f"Servo {e.id_} is not responding. Exiting...")
@@ -43,26 +43,39 @@ def keyframe(m1,m2,n):
     #loc defines the location of the leg being tested (FL, BL, FR, BR)
 
     #Disable torque to enable keyframe
-    m1.disable_torque()
-    m2.disable_torque()
+    m1.enable_torque()
+    m2.enable_torque()
     #Indicate Motors are prepared for keyframing
     m1.led_power_on()
     m2.led_power_on()
     motor_A = []
     motor_B = []
+    if m1 == servoL1:
+        print("Left Front Leg")
+
+    if m1 == servoL3:
+        print("Left Back Leg")
+
+    if m1 == servoR101:
+        print("Right Front Leg")
+        
+    if m1 == servoR103:
+        print("Right Back Leg")
 
     for i in range(n):
-
+        input()
         input("Position the leg to the keyframe position, Press Enter to record its angles.")
         motor_A += [i,m1.get_physical_angle()]
-        motor_B += [i,m1.get_physical_angle()]
+        motor_B += [i,m2.get_physical_angle()]
+        print(m1.get_physical_angle())
+        print(m2.get_physical_angle())
 
-    #reenable torque in the motors
-    m1.enable_torque()
-    m2.enable_torque()
     print([motor_A])
     print([motor_B])
     return motor_A, motor_B
 
 #Servo 1 & 2 Key Framing (FRONT LEFT LEG)
-keyframe(servoL1,servoL2,5)
+keyframe(servoL1,servoL2, 5)
+keyframe(servoL3, servoL4, 5)
+keyframe(servoR101, servoR102, 5)
+keyframe(servoR103, servoR104, 5)
